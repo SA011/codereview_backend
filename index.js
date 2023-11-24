49152b674b7951169e8c32d15d99ba1a8368f2e5
SA011 <60836699+SA011@users.noreply.github.com>
2023-11-24 22:00:13 +0600

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8080;
 const app = Express();
 
 app.use(cors({
-    origin: process.env.ORIGIN
+    origin: "*"
 }));
 app.use(Express.json());
 app.use('/api',router);
