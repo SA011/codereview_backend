bf773a7ce379f621d99b10956588430f6a9859e4
SA011 <pritomsabit@gmail.com>
2024-09-14 16:16:31 +0600

    update

--- database/connect.js
+++ database/connect.js
@@ -5,7 +5,8 @@ const config = {
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
-    port: process.env.PGPORT
+    port: process.env.PGPORT,
+    ssl: true
 };
 module.exports.getConnection = async () => {
     try {
